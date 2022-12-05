56173fdf3b117f5a062566a64160689bfa13dabe
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-12-05 23:00:04 +0100

    created working files for project

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{fontSize: '4rem'}}>Welcome to Auth_Wiki!</h1>
+    </div>
+  );
+}
+
+export default App;
