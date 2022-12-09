d5c39642bbbc5006634670658046ecd9b0a2709d
LeRoi Vladimir <banyamkimbi@vrfound.org>
2022-12-09 15:56:24 +0100

    before pull

--- FrontEnd/src/pages/Library/Library.js
+++ FrontEnd/src/pages/Library/Library.js
@@ -21,8 +21,10 @@ function Library() {
       <div className='doc'>
       <div className='doc-header'>
         <div className='left'>
+          <div className='doc-desc'>
           <h3>Authwiki Library</h3>
           <p>A Comprehensive page on Auth-wiki authentication codes, documentation and related information.</p>
+          </div>
         </div>
         <div className='right'>
           <img src={logo} alt='code-editor' className='img1' />
