4923b35bcd71787398653f513ea0d8034bd01d7d
Omosaye Moses Iseoluwa <77594582+theselaw@users.noreply.github.com>
2022-12-10 20:49:47 -0800

    Added the body for the sign up page

--- FrontEnd/src/pages/Signup page/Signup.js
+++ FrontEnd/src/pages/Signup page/Signup.js
@@ -5,27 +5,29 @@ import "./signup.css";
 
 export default function Signup() {
   return (
-    <section>
-      {/* Main container for all codes*/}
-      <div className="Container">
-        {/* left side of the form*/}
-        <div className='Col-1'>
-          <img src={colImg} alt="" />
-          </div>
-          
-          {/*The actual form*/}
+    <main>
+    <div class="Main-Container">
+    <div class="Left-Form-Container">
+    <img class="Icon" src="/Assets/signin.png" alt="" >
 
-          <div className='Col-2'>
-          <div className='TextContainer'>
-          <img  />
-            <h1>Sign Up </h1>
-          </div>
-            <form>
-            <input type="text" placeholder='username' />
-            </form>
-          </div>
-      </div>
-      </div>
-    </section>
+    </div>
+    <div class="Right-Form-Container">
+<img class="logo" src="/Assets/Authwiki-red.png" alt="logo"/>
+        <h2>Sign Up</h2>
+        <div class="hrwrapper"><hr id="hrline"/>OR LOGIN WITH EMAIL<hr id="hrline" /></div>
+<form>
+    <input id="inputs" placeholder="Your Name" type="text">
+        <input id="inputs" type="email" placeholder="Your Email"/>
+        <input id="inputs" type="password" placeholder="Your Password"/>
+        <div class="lower-link"><div class="Remember-Me"><input type="checkbox"><span>I agree to the </span></div>  <div class="forgotten"><span><a href="">I agree to the Terms and Conditions</a></span></div></div>    
+        <button id="submit" type="submit">Sign Up</button>
+    </form>
+    <hr/>
+    <div class="base-link">Already have an account? <a href="">  Log In</a></div>
+    <div class="UpperLink"><img class="Google-icon"src="/Assets/google.png" alt=""> <span class="alt-Auth">Log in with Google</span></div>
+    </div>
+
+    </div>
+    </main>
   )
 }
