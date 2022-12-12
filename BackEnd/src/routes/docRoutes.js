4acf979aaec3fd49db2e8f7239149267173ab128
Oluwasijibomi Solola <oluwasijibomisolola@gmail.com>
2022-12-12 19:45:52 +0100

    Reaction Endpoints, to like and dislike.

--- BackEnd/src/routes/docRoutes.js
+++ BackEnd/src/routes/docRoutes.js
@@ -1,11 +1,15 @@
-const { getAllDocs, getSingleDoc, downloadDoc } = require("../controllers/docController");
+const { getAllDocs, getSingleDoc, downloadDoc, docReaction } = require("../controllers/docController");
+
+const {authenticate} = require('../middleware/authMiddleware');
+
 const express = require("express");
 const router = express.Router();
 
 
 router.get('/', getAllDocs);
 router.get('/:id', getSingleDoc);
-router.get('/download/:id', downloadDoc)
+router.get('/download/:id', authenticate,  downloadDoc);
+router.patch('/:id', authenticate, docReaction);
 
 
 module.exports = router
