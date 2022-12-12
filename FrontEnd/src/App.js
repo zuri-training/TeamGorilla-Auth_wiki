4b3755744a79a91ea26e3f6a2e5886eba961c9f9
LeRoi Vladimir <banyamkimbi@vrfound.org>
2022-12-12 16:15:37 +0100

    nothing

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -8,23 +8,31 @@ import FAQ from './pages/FAQ/FAQ';
 import Home from './pages/Home/Home';
 import Team from './pages/Team/Team';
 import AuthLibrary from './pages/Library/AuthLibrary';
-import Login from './pages/Login page/Login'
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import Reset from './pages/Reset/Reset';
+import Verification from './pages/Verification/Verification';
 
 function App() {
   return (
     <div className="App">
       <Header />
        {/* <Footer />  */}
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route index element={<Home/>}/>
           <Route path='/library' element={<Library/>}/>
           <Route path='/faq' element={<FAQ/>}/>
-          <Route path='/team' element={<Team/>}/>          
+          <Route path='/team' element={<Team/>}/>  
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Signup/>} />
+          <Route path='/Reset' element={<Reset/>} />
+          <Route path='/verification' element={<Verification/>}/>
+          <Route path='/library/auth' element={<AuthLibrary/>} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
       {/* <AuthLibrary /> */}
-      <Login />
+      {/* <Login /> */}
       <Footer /> 
     </div>
   );
