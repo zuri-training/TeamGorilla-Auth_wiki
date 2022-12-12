01e19547db6504b57d7a1b2dcb7311288794d4a5
Ehidiamhen <ehidiamhenaigbiluese@gmail.com>
2022-12-12 16:22:04 +0100

    feat: completed team page

--- FrontEnd/src/pages/Team/Team.jsx
+++ FrontEnd/src/pages/Team/Team.jsx
@@ -1,10 +1,127 @@
 import React from 'react'
 import './Team.css'
+import Template from './template/template'
 
 export default function Team () {
     return (
-        <div>
-            <h1>Team Gorrila Members:</h1>
+        <div className='team'>
+            <header className='heading'>
+              <h1>Our Team Members</h1>
+              <p>Meet our team of developers, designers, <br/> and world-class problem solvers.</p>
+            </header>
+            <div className='team-members'>
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849525/GORILLA/IMG_20220222_093424_596_-_Brandon_Armand_kdj1yk.jpg'
+                alt='team-member-1'
+                name='Brandon Armand Nyamkimbi'
+                role='Fullstack Developer'
+                status='Team Lead'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849526/GORILLA/profile_image_-_Ogunlalu_Ademola_abminb.jpg'
+                alt='team-member-2'
+                name='Ogunlalu Ademola'
+                role='Product Designer'
+                status='Asst. Team Lead'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849526/GORILLA/profile_image_-_Ogunlalu_Ademola_abminb.jpg'
+                alt='team-member-3'
+                name='Oluwasijibomi Solola'
+                role='Fulstack Developer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849526/GORILLA/IMG_20221129_102615_-_Olubimi_Michael_kcm84w.jpg'
+                alt='team-member-4'
+                name='Olubimi Michael Olumide'
+                role='Fullstack Developer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849526/GORILLA/my_passport_-_OLAYINKA_SHEBA_tc6lsi.jpg'
+                alt='team-member-5'
+                name='Olayinka Ismaila'
+                role='Product Designer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849524/GORILLA/IMG_20210905_184727_944_-_Animadu_Blessing_tzfjjp.webp'
+                alt='team-member-6'
+                name='Blessing Ogwu'
+                role='Product Designer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849523/GORILLA/EF6A9F73-06F4-4370-8A98-CAE17B0DA23E_-_Erickson_Akhabue_trqea5.jpg'
+                alt='team-member-7'
+                name='Erickson Akhabue'
+                role='Product Designer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849524/GORILLA/Grace_Eyiolawi_-_Grace_Eyiolawi_nyq8nb.jpg'
+                alt='team-member-8'
+                name='Grace Eyiolawi'
+                role='Product Designer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849524/GORILLA/Grace_Eyiolawi_-_Grace_Eyiolawi_nyq8nb.jpg'
+                alt='team-member-9'
+                name='Ebiloma Aliyu'
+                role='Product Designer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849523/GORILLA/426F394E-B334-419E-A8EB-EAB9B37C40A8_-_Omua_Dominion_tjxahz.jpg'
+                alt='team-member-10'
+                name='Owiku Dominion Omua'
+                role='Product Designer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849523/GORILLA/1662589470483_-_Ajayi_Victor_stve5r.jpg'
+                alt='team-member-11'
+                name='Victor Ajayi'
+                role='Product Designer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670857556/GORILLA/3DAFF248-C6D2-437D-9051-4BE90D3CA161_-_Obe_Tessy_udsymm.jpg'
+                alt='team-member-12'
+                name='Obe Theresa Onyeka'
+                role='Product Designer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849523/GORILLA/1662589470483_-_Ajayi_Victor_stve5r.jpg'
+                alt='team-member-13'
+                name='Aminat Abdulwaheed'
+                role='Product Designer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849524/GORILLA/IMG-20221121-WA0021_-_FRIDAH_JOSEPHINE_KAWALA_fzxlj7.jpg'
+                alt='team-member-14'
+                name='Kawala Fridah Josephine'
+                role='Product Designer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849523/GORILLA/20221007_164242_-_Ehidiamhen_Aigbiluese_elhvnl.jpg'
+                alt='team-member-15'
+                name='Aigbiluese Ehidiamhen'
+                role='Frontend Developer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849525/GORILLA/IMG_20210610_103700_550_2_-_Kingsblog_network_grpr2f.jpg'
+                alt='team-member-16'
+                name='Omosaye Moses Iseoluwa'
+                role='Frontend Developer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849524/GORILLA/IMG-20221210-WA0026_-_Nwaoghor_Praise_ngjtz9.jpg'
+                alt='team-member-17'
+                name='Nwaoghor Praise Chukunweiken'
+                role='Backend Developer'
+              />
+              <Template
+                src='https://res.cloudinary.com/dbfqhe9vc/image/upload/v1670849524/GORILLA/IMG-20221210-WA0026_-_Nwaoghor_Praise_ngjtz9.jpg'
+                alt='team-member-18'
+                name='Olatunji Oluwabamise'
+                role='Backend Developer'
+              />
+            </div>
+            
         </div>
     )
 }
