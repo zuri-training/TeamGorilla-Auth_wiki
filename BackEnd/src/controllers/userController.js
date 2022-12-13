e7b435a6ccd4c4971092e0b33a4b1ec38abd09f2
Brandon Armand Nyamkimbi <111974934+LeRoiCodes@users.noreply.github.com>
2022-12-13 15:03:57 +0100

    Update userController.js

--- BackEnd/src/controllers/userController.js
+++ BackEnd/src/controllers/userController.js
@@ -45,7 +45,7 @@ const registerUser = asyncHandler( async (req, res) => {
 			const text = `<h1>Email Confirmation</h1>
         <h2>Hello ${firstName}</h2>
         <p>Verify your email address to complete the signup and login to your account to Authwiki</p>
-        <a href='http://localhost:8000/api/user/register/${user.verificationCode}'> Click here</a>
+        <a href='https://auth-wiki.onrender.com/api/user/register/${user.verificationCode}'> Click here</a>
 
         </div>`;
 
@@ -247,7 +247,7 @@ const forgotPassword = asyncHandler(async (req, res) => {
         <h2>Hello ${user.firstName}</h2>
         <p>You are receiving this email because you (or someone else) has
          requested the reset of a password</p>
-           <a href='http://localhost:8000/api/user/resetpassword/${resetToken}'> Click here to reset your password</a>
+           <a href='https://auth-wiki.onrender.com/api/user/resetpassword/${resetToken}'> Click here to reset your password</a>
 
         </div>`;
 
