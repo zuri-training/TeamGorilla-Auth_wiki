368c5785959c98ff08f665d02d23bac825cd3f30
Olubimi Michael <Olubimimichael01@gmail.com>
2022-12-16 18:16:39 +0000

    Update 4 files

--- FrontEnd/src/pages/Home/Home.jsx
+++ FrontEnd/src/pages/Home/Home.jsx
@@ -7,12 +7,12 @@ import partner1 from "../../assets/images/partner1.png";
 import partner2 from "../../assets/images/partner2.png";
 import partner3 from "../../assets/images/slack.png";
 import partner4 from "../../assets/images/Vector.png";
-import about from "../../assets/images/about-us.png"
-import user_1 from "../../assets/images/user_1.png"
+import about from "../../assets/images/about-us.png";
+import user_1 from "../../assets/images/user_1.png";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import { FaRegFileAlt } from "react-icons/fa";
 import { FaUsers } from "react-icons/fa";
-import { BsArrowRight } from "react-icons/bs"
+import { BsArrowRight } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -30,17 +30,17 @@ export default function Home() {
         </button>
       </div>
       <div className="codes">
-      <div className="code-codes">
-        <h1 className="code1">
-          <img src={code3} alt="main" />
-        </h1>
-        <h1 className="code2">
-          <img src={code1} alt="main" />
-        </h1>
-        <h1 className="code3">
-          <img src={code2} alt="main" />
-        </h1>
-      </div>
+        <div className="code-codes">
+          <h1 className="code1">
+            <img src={code3} alt="main" />
+          </h1>
+          <h1 className="code2">
+            <img src={code1} alt="main" />
+          </h1>
+          <h1 className="code3">
+            <img src={code2} alt="main" />
+          </h1>
+        </div>
       </div>
       <div className="brand">
         <div className="brands-with">
@@ -104,65 +104,78 @@ export default function Home() {
         </div>
       </div>
 
-      <section className='home_about'>
-      <div className="about_section">
-         <img src={about} alt="about-us" className="about_img"/>
-         <div className="about_text">
+      <section className="home_about">
+        <div className="about_section">
+          <img src={about} alt="about-us" className="about_img" />
+          <div className="about_text">
             <h2>About Us</h2>
             <p>Find out about who we are</p>
-            <p style={{padding: '1rem 0 3rem 0'}}>
-               We are a group of tech savvy individuals with an aim to elevate the technology industry
-               through speed and agility. One of the solutions we are proud to offer is our authentication library.
-               Our team is made up of tech enthusiast with a forward driven mind set.
+            <p style={{ padding: "1rem 0 3rem 0" }}>
+              We are a group of tech savvy individuals with an aim to elevate
+              the technology industry through speed and agility. One of the
+              solutions we are proud to offer is our authentication library. Our
+              team is made up of tech enthusiast with a forward driven mind set.
             </p>
             {/* <Link to='/team' className="about_link">Find Out More <BsArrowRight className="about-arrow"/></Link> */}
-         </div>
-      </div>
-
+          </div>
+        </div>
       </section>
       <div className="testimonial_section">
-         <h2>Our Happy Users</h2>
-         <div className="testimonials__">
-           <div className="testimonial">
-              <img src={user_1} alt="user-1" />
-              <p style={{fontWeight: '700'}}>David Mark</p>
-              <p>
-                The days of coding from scratch are over. Authwiki greatly reduces turn around time.
-                I will definitely reccommend this to any serious developer out there
-              </p>
-           </div>
-           <div className="testimonial">
-              <img src={user_1} alt="user-1" />
-              <p style={{fontWeight: '700'}}>David Mark</p>
-              <p>
-                The days of coding from scratch are over. Authwiki greatly reduces turn around time.
-                I will definitely reccommend this to any serious developer out there
-              </p>
-           </div>
-           <div className="testimonial">
-              <img src={user_1} alt="user-1" />
-              <p style={{fontWeight: '700'}}>David Mark</p>
-              <p>
-                The days of coding from scratch are over. Authwiki greatly reduces turn around time.
-                I will definitely reccommend this to any serious developer out there
-              </p>
-           </div>
-         </div>
+        <h2>Our Happy Users</h2>
+        <div className="testimonials__">
+          <div className="testimonial">
+            <img src={user_1} alt="user-1" />
+            <p style={{ fontWeight: "700" }}>David Mark</p>
+            <p>
+              The days of coding from scratch are over. Authwiki greatly reduces
+              turn around time. I will definitely reccommend this to any serious
+              developer out there
+            </p>
+          </div>
+          <div className="testimonial">
+            <img src={user_1} alt="user-1" />
+            <p style={{ fontWeight: "700" }}>David Mark</p>
+            <p>
+              The days of coding from scratch are over. Authwiki greatly reduces
+              turn around time. I will definitely reccommend this to any serious
+              developer out there
+            </p>
+          </div>
+          <div className="testimonial">
+            <img src={user_1} alt="user-1" />
+            <p style={{ fontWeight: "700" }}>David Mark</p>
+            <p>
+              The days of coding from scratch are over. Authwiki greatly reduces
+              turn around time. I will definitely reccommend this to any serious
+              developer out there
+            </p>
+          </div>
+        </div>
       </div>
 
       {/* <section className='newsletter'> */}
       <div className="newsletter_section">
-         <div className="newsletter_text">
-           <h3 style={{fontSize: '24px'}}>Do you want to get updates from AuthWiki?</h3>
-           <p style={{fontSize: '18px', margin: '0'}}>Sign up for Our Newsletter</p>
-         </div>
-         <div className="newsletter_input">
-            <input type="email" name="email" id="email" placeholder="enter your email address" size="50" className="newsletter_email" />
-            <BsArrowRight className="newsletter_arrow"  />
-         </div>
-         </div>
+        <div className="newsletter_text">
+          <h3 style={{ fontSize: "24px" }}>
+            Do you want to get updates from AuthWiki?
+          </h3>
+          <p style={{ fontSize: "18px", margin: "0" }}>
+            Sign up for Our Newsletter
+          </p>
+        </div>
+        <div className="newsletter_input">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="enter your email address"
+            size="50"
+            className="newsletter_email"
+          />
+          <BsArrowRight className="newsletter_arrow" />
+        </div>
+      </div>
       {/* </section> */}
-      
     </div>
   );
 }
